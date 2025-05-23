5b2e5de2c3f4ddd42fd0fd9a99635e2f00fd815e
Erfan <erfansafari.b.86@gmail.com>
2025-05-23 19:28:30 +0300

    resume page finished

--- Personal_Web/core/admin.py
+++ Personal_Web/core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage,ResumeEntry,CertificateEntry
 admin.site.register(ContactMessage)
+admin.site.register(ResumeEntry)
+admin.site.register(CertificateEntry)
+
 # Register your models here.
--- Personal_Web/core/urls.py
+++ Personal_Web/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',Home.as_view(),name='home'),
@@ -7,3 +9,5 @@ urlpatterns=[
     path('resume/',Resume.as_view(),name='resume'),
     path('contact/',Contact.as_view(),name='contact'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
