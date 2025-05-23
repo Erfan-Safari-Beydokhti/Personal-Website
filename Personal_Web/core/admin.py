98e2a199ea801e713e792df1e86784fd4402ef5e
Erfan <erfansafari.b.86@gmail.com>
2025-05-23 20:14:58 +0300

    edit home.html

--- Personal_Web/core/admin.py
+++ Personal_Web/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ContactMessage,ResumeEntry,CertificateEntry
+from .models import ContactMessage,ResumeEntry,CertificateEntry,AboutMe
+
 admin.site.register(ContactMessage)
 admin.site.register(ResumeEntry)
 admin.site.register(CertificateEntry)
-
+admin.site.register(AboutMe)
 # Register your models here.
