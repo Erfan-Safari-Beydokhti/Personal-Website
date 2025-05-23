19d1e6be6fd31776d38b030ab1ab20e69bd61560
Erfan <erfansafari.b.86@gmail.com>
2025-05-23 19:44:39 +0300

    Create Form for about page

--- Personal_Web/core/forms.py
+++ Personal_Web/core/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import ContactMessage
+from .models import ContactMessage,AboutMe
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name','email','message']
+
+class AboutForm(forms.ModelForm):
+    class Meta:
+        model = AboutMe
+        fields = ['name','bio','profile_picture','skills']
+
